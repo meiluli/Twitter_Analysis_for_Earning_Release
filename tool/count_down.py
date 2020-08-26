96145f2d31739ed8bff94bdd182d80cb86358dce
Meilu Li <ml6684@nyu.edu>
2020-08-26 12:45:43 -0400

    twitter limits waiting tool

---
+++ tool/count_down.py
@@ -0,0 +1,15 @@
+import time
+
+
+def countdown(t):
+    """
+    wait for t mins
+    """
+    print(f"Wait for {t} min...")
+    t=int(t*60+1)
+    while t:
+        mins, secs = divmod(t, 60)
+        timeformat = '{:02d}:{:02d}'.format(mins, secs)
+        print(timeformat, end='\r')
+        time.sleep(1)
+        t -= 1
